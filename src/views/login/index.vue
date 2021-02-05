63c41836f7b8e70335e027377e5fc90922fcf776
lin93h <1098039601@qq.com>
2021-02-05 17:16:02 +0800

    初始化项目

---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login-box">
+      <div class="box-title box-title box-titlebox-title box-title box-title box-title box-title box-title box-title box-title box-title">
+        vue3-TS-ElementPlus后台管理系统</div>
+      <div class="login-form">
+        <el-input v-model="account"></el-input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Login',
+  data() {
+    return {
+      account: ''
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.login-wrapper {
+  height: 100%;
+  background: #fff;
+  overflow: hidden;
+}
+</style>
