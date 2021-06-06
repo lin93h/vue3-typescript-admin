b29aefbc4964e063175bbcbbf7830de36b91d191
linhuang <1098039601@qq.com>
2021-06-07 00:50:41 +0800

    状态管理类型定义

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,30 +1,28 @@
-import { Mutation, ActionHandler } from 'vuex'
+import { MutationTree, ActionTree } from 'vuex'
 import { getToken } from '@/utils/cookie'
 
 export interface UserState {
-  token: string | undefined | null,
+  token: string | undefined | null
   userInfo: Record<string, unknown> | null
 }
-
-type MuType = Mutation<UserState>
 
 const state: UserState = {
   token: getToken(),
   userInfo: null
 }
 
-
-const mutations = {
+const mutations: MutationTree<UserState> = {
   setToken(state: UserState, token: string): void {
     state.token = token
   }
 }
 
-const actions = {
-  setToken({ commit: (x: string, payload: unknown) => void } : { commit }, token: string) {
-  commit('setToken', token)
+const actions: ActionTree<UserState, unknown> = {
+  setToken({ commit }, token: string) {
+    commit('setToken', token)
+  }
 }
-}
+
 export default {
   namespaced: true,
   state,
