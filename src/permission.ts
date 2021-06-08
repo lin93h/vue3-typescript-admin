b20f2df48140c7bfe03f45184f6305ca7eceeec5
linhuang <1098039601@qq.com>
2021-06-08 23:08:40 +0800

    状态管理

--- src/permission.ts
+++ src/permission.ts
@@ -1,7 +1,8 @@
 import router from './router'
-// import store from '@/store/index'
+import store from '@/store/index'
 
 router.beforeEach((from, to, next) => {
-  console.log(from, to)
+  const token = store.getters.token
+  console.log(token, from, to)
   next()
 })
--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,10 +1,11 @@
 import { UserState } from './modules/user'
+import { GetterTree } from 'vuex'
 
 interface StateType {
   user: UserState
 }
-const getters = {
-  token: (state: StateType) => state.user.token
+const getters: GetterTree<StateType, unknown> = {
+  token: (state) => state.user.token
 }
 
 export default getters
