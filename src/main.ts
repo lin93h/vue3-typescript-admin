4753cd92f4a0c18ec75313d1e3c4b87545c43979
linhuang <1098039601@qq.com>
2022-05-24 17:31:46 +0800

    引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// 全量引入
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// 按需引入
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +15,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// 全量引入
+// app.use(ElementPlus)
+
+// 按需引入
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
