40ee26f31d0e686433b0d17311cb3ec652a15573
linhuang <1098039601@qq.com>
2022-06-14 18:00:10 +0800

    全量引入样式，按钮引入ElMessageBox样式无法加载

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 // 全量引入
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 // 按需引入
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'element-plus/theme-chalk/dark/css-vars.css'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import 'element-plus/theme-chalk/dark/css-vars.css'
 
 import App from './App.vue'
 import router from './router'
@@ -16,11 +16,11 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 // 全量引入
-// app.use(ElementPlus)
+app.use(ElementPlus)
 
 // 按需引入
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//   app.component(key, component)
+// }
 
 app.mount('#app')
